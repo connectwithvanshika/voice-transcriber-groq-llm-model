b8792b384018c9c1d07519b326fe54f6e6e20e40
Vanshika <vanshika.connects@gmail.com>
2026-06-16 15:16:43 +0530

    response from ai

--- main.py
+++ main.py
@@ -58,3 +58,22 @@ Transcript:
 
  """
 
+response = client.chat.completions.create(
+            model="llama-3.3-70b-versatile",
+            messages=[
+                {
+                    "role": "system",
+                    "content": "You are an expert conversation analyzer."
+                },
+                {
+                    "role": "user",
+                    "content": prompt
+                }
+            ],
+            temperature=0
+        )
+
+speaker_transcript = response.choices[0].message.content
+
+print("\nSpeaker Transcript:\n")
+print(speaker_transcript)
